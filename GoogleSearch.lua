9cdb93b961579e3e8fa0d978f56e2d753c9e0dfd
Matt Calsada <mcalsada@atlas-sys.com>
2018-12-13 14:56:07 -0500

    Update to v1.1

--- GoogleSearch.lua
+++ GoogleSearch.lua
@@ -2,29 +2,41 @@
 --
 -- GoogleSearch.lua does a generic Google search for the LoanTitle for loans and the PhotoArticleTitle for articles.
 -- autoSearch (boolean) determines whether the search is performed automatically when a request is opened or not.
---
--- "f" is the formname on google.com
--- "q" is the text box name on google.com
 
-local autoSearch = GetSetting("AutoSearch");
+local settings = {};
+settings.AutoSearch = GetSetting("AutoSearch");
 
 local interfaceMngr = nil;
-local browser = nil;
+local googleSearchForm = {};
+googleSearchForm.Form = nil;
+googleSearchForm.Browser = nil;
+googleSearchForm.RibbonPage = nil;
+
+require "Atlas.AtlasHelpers";
 
 function Init()
-	LogDebug("Script initialized dustin");
 	interfaceMngr = GetInterfaceManager();
 	
-	-- Create browser		
-	browser = interfaceMngr:CreateBrowser("Google Search", "Google Search", "Script");
+	-- Create a form
+	googleSearchForm.Form = interfaceMngr:CreateForm("Google Search", "Script");
 	
-	-- Create buttons
-	browser:CreateButton("Search", GetClientImage("Search32"), "Search", "Google");
+	-- Add a browser
+	googleSearchForm.Browser = googleSearchForm.Form:CreateBrowser("Google Search", "Google Search Browser", "Google Search");
 	
-	browser:Show();
+	-- Hide the text label
+	googleSearchForm.Browser.TextVisible = false;
+	
+	-- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  We can retrieve that one and add our buttons to it.
+	googleSearchForm.RibbonPage = googleSearchForm.Form:GetRibbonPage("Google Search");
+	
+	-- Create the search button
+	googleSearchForm.RibbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "Google");
+	
+	-- After we add all of our buttons and form elements, we can show the form.
+	googleSearchForm.Form:Show();
 	
 	-- Search when opened if autoSearch is true
-	if autoSearch then
+	if settings.AutoSearch then
 		Search();
 	end
 end
@@ -38,6 +50,6 @@ function Search()
 		searchText = GetFieldValue("Transaction", "PhotoArticleTitle");
     end
 	
-	browser:Navigate("http://google.com/search?q=" .. searchText);	
+	googleSearchForm.Browser:Navigate("http://google.com/search?q=" .. AtlasHelpers.UrlEncode(searchText));
 end
 
